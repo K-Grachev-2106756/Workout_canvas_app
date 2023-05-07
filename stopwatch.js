40868c52a19512203488ccbb94d9b6698d88178e
kirill737 <91204221+kirill737@users.noreply.github.com>
2023-05-07 22:34:29 +0300

    Add files via upload

--- stopwatch.js
+++ stopwatch.js
@@ -1,24 +1,51 @@
-let intervalId;
-let seconds = 0;
-let minutes = 0;
-let hours = 0;
+let timerInterval;
+let seconds = 0, minutes = 0, hours = 0;
+const stopwatch = document.getElementById("time");
+const startBtn = document.getElementById("start");
+const resetBtn = document.getElementById("reset");
 
 function startTimer() {
-  intervalId = setInterval(function() {
-    seconds++;
-    if (seconds === 60) {
-      seconds = 0;
-      minutes++;
-      if (minutes === 60) {
-        minutes = 0;
-        hours++;
-      }
-    }
-    let timerDisplay = document.getElementById("stopwatch");
-    timerDisplay.innerHTML = `${hours.toString().padStart(2, '0')}:${minutes.toString().padStart(2, '0')}:${seconds.toString().padStart(2, '0')}`;
-  }, 1000);
+	startBtn.textContent = "STOP";
+	startBtn.id = ("start");
+	startBtn.id = ("stop");
+	startBtn.removeEventListener("click", startTimer);
+	startBtn.addEventListener("click", stopTimer);
+
+	timerInterval = setInterval(function() {
+		seconds++;
+		if (seconds == 60) {
+			seconds = 0;
+			minutes++;
+		}
+		if (minutes == 60) {
+			minutes = 0;
+			hours++;
+		}
+		stopwatch.textContent = pad(hours) + ":" + pad(minutes) + ":" + pad(seconds);
+	}, 1000);
 }
 
 function stopTimer() {
-  clearInterval(intervalId);
+	startBtn.textContent = "START";
+	startBtn.id = ("stop");
+	startBtn.id = ("start");
+	startBtn.removeEventListener("click", stopTimer);
+	startBtn.addEventListener("click", startTimer);
+
+	clearInterval(timerInterval);
 }
+
+function resetTimer() {
+	seconds = 0;
+	minutes = 0;
+	hours = 0;
+	stopwatch.textContent = "00:00:00";
+	stopTimer();
+}
+
+function pad(num) {
+	return num < 10 ? "0" + num : num;
+}
+
+startBtn.addEventListener("click", startTimer);
+resetBtn.addEventListener("click", resetTimer);
